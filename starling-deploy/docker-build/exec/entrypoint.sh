347c629a961635bfd792c3cf1cd73502d3d757cd
Ryan Kuederle <ryan@rkuederle.com>
2025-07-23 18:12:34 -0400

    run ros in entrypoint

--- starling-deploy/docker-build/exec/entrypoint.sh
+++ starling-deploy/docker-build/exec/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 source /opt/ros/humble/setup.bash
-source /csi_ros/install/setup.bash
 source /opt/ros2-csi-msgs/install/setup.bash
+cd /csi_ros
+colcon build --install-base install
+source /csi_ros/install/setup.bash
 ros2 run csi_ros csi_pub
