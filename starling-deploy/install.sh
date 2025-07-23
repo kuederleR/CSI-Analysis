084735904e713bc9dc8bf5cd354d96d5bc0ce0a7
Ryan Kuederle <ryan@rkuederle.com>
2025-07-23 18:16:09 -0400

    created systemd install

---
+++ starling-deploy/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+SERVICE_NAME=starling-docker
+WORKDIR="$(dirname $(realpath $0))"
+
+# Create systemd service file
+cat <<EOF | sudo tee /etc/systemd/system/$SERVICE_NAME.service
+[Unit]
+Description=CSI ROS2 Docker Compose Service
+After=network.target
+Requires=docker.service
+
+[Service]
+Type=simple
+WorkingDirectory=$WORKDIR
+ExecStart=/usr/bin/docker compose up
+ExecStop=/usr/bin/docker compose down
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload systemd, enable and start the service
+sudo systemctl daemon-reload
+sudo systemctl enable $SERVICE_NAME
+sudo systemctl start $SERVICE_NAME
+
+echo "Systemd service '$SERVICE_NAME' installed and started."
