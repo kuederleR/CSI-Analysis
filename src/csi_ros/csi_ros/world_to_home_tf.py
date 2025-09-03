119633ccfdb6eb955bc3c0f0c8726de530f2db25
Ryan Kuederle <ryan@rkuederle.com>
2025-09-03 13:23:35 -0400

    use vectory and quat data tyypes

--- src/csi_ros/csi_ros/world_to_home_tf.py
+++ src/csi_ros/csi_ros/world_to_home_tf.py
@@ -100,9 +100,9 @@ class TransformManager(Node):
 
             # position = [msg.pose.position.x, msg.pose.position.y, msg.pose.position.z]
             # orientation = [msg.pose.orientation.w, msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z]
-            _, _, yaw = quat2euler(orientation[0], orientation[1], orientation[2], orientation[3])
+            _, _, yaw = quat2euler(orientation.w, orientation.x, orientation.y, orientation.z)
             yaw_deg = math.degrees(yaw)
-            self.set_home(position, yaw_deg)
+            self.set_home([position.x, position.y, position.z], yaw_deg)
 
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
             self.get_logger().error(f"Error in home_pose_callback transform: {e}")
